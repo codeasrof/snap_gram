8ad8a98834ca4cc2f0f8696f6a31e6b600fd0b5a
codeasrof <vitalidade.08-devorador@icloud.com>
2023-11-04 15:53:19 -0300

    basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import SigninForm from "./_auth/forms/SigninForm"
+import SignupForm from "./_auth/forms/SignupForm"
+import { Home } from "./_root/pages"
 import "./globals.css"
+import {Routes, Route} from "react-router-dom"
 
 const App = () => {
   return (
-    <div>
-        <h1 className="text-3xl font-bold text-white">Hi</h1>
-    </div>
+    <main className="flex h-screen">
+        <Routes>
+
+            {/* Public routes */}
+            <Route path="/sign-in" element={<SigninForm/>} />
+            <Route path="/sign-up" element={<SignupForm/>} />
+
+            {/* Private Routes */}
+            <Route index element={<Home/>} />
+            
+        </Routes>
+    </main>
   )
 }
 
