2a5415dedbf27bdc298f097f0352c089cfc54f74
codeasrof <vitalidade.08-devorador@icloud.com>
2023-11-06 22:36:47 -0300

    adding shadCN

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import AuthLayout from "./_auth/AuthLayout"
 import SigninForm from "./_auth/forms/SigninForm"
 import SignupForm from "./_auth/forms/SignupForm"
+import RootLayout from "./_root/RootLayout"
 import { Home } from "./_root/pages"
 import "./globals.css"
 import {Routes, Route} from "react-router-dom"
@@ -10,12 +12,16 @@ const App = () => {
         <Routes>
 
             {/* Public routes */}
-            <Route path="/sign-in" element={<SigninForm/>} />
-            <Route path="/sign-up" element={<SignupForm/>} />
+            <Route element={<AuthLayout/>}>
+                <Route path="/sign-in" element={<SigninForm/>} />
+                <Route path="/sign-up" element={<SignupForm/>} />
+            </Route>
 
             {/* Private Routes */}
-            <Route index element={<Home/>} />
-            
+            <Route element={<RootLayout/>}>
+                <Route index element={<Home/>} />
+            </Route>
+
         </Routes>
     </main>
   )
